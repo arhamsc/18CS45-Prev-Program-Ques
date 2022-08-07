06e622c91057052c4b81c8087d086508441ac505
Sagar6250 <sagars6250@gmail.com>
2022-08-07 20:23:24 +0530

    Swings Q1

---
+++ src/com/swingsQ1/ComboBoxExample.java
@@ -0,0 +1,33 @@
+
+//Question:
+//	Explain JCombobox with example
+	
+//JComboBox: The object of Choice class is used to show popup menu of choices. 
+//Choice selected by user is shown on the top of a menu. It inherits JComponent class.
+
+import javax.swing.*;    
+
+public class ComboBoxExample {    
+	//frame
+	JFrame f;    
+	ComboBoxExample(){    
+	    //create a new frame
+		f=new JFrame("ComboBox Example");    
+	    
+	    String country[]={"India","Aus","U.S.A","England","Newzealand"};        
+	    
+	    //create Check Box
+	    JComboBox cb=new JComboBox(country);    
+	    cb.setBounds(100, 100,150,20);    
+	    
+	    //add Combo box to frame
+	    f.add(cb);        
+	    f.setLayout(null);    
+	    //set size of the frame
+	    f.setSize(400,400);    
+	    f.setVisible(true);         
+	}    
+	public static void main(String[] args) {    
+	    new ComboBoxExample();         
+	}    
+}  
