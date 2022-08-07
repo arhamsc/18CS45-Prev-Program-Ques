9240750e5c752387d1fe32facf0cdc8441d3583c
RayeesHussain <96644069+RayeesHussain@users.noreply.github.com>
2022-08-07 20:29:22 +0530

    Create MyClass.java

---
+++ src/com/pckgsQ1/MyClass.java
@@ -0,0 +1,15 @@
+/*- User-defined-package:- Java package created by user to categorized classes and interface. 
+  These are the packages that are defined by the user.
+  Eg program-First we create a directory myPackage (name should be same as the name of the package). 
+  Then create the MyClass inside the directory with the first statement being the package names.*/
+// Name of the package must be same as the directory
+// under which this file is saved
+package myPackage;
+
+public class MyClass
+{
+    public void getNames(String s)
+    {        
+        System.out.println(s);        
+    }
+}
