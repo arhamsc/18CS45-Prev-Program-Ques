5e641d3c26a54e062bcfa9b7d52717b74c81d81b
SiddharthB70 <siddharth7002b@gmail.com>
2022-08-09 16:54:39 +0530

    Fixed JComboBox Type Parameter

--- src/com/swingsQ6/JComboBoxExample.java
+++ src/com/swingsQ6/JComboBoxExample.java
@@ -6,12 +6,12 @@ import javax.swing.JFrame;
 import javax.swing.JComboBox;    
 public class JComboBoxExample {    
     JFrame f; 
-    JComboBox cb;   
+    JComboBox<String> cb;   
     JComboBoxExample(){    
         f = new JFrame();
         f.setTitle("Fruits");    
         String fruits[] = {"Apple","Orange","Mango","Grapes"};        
-        cb = new JComboBox(fruits);    
+        cb = new JComboBox<String>(fruits);    
         cb.setBounds(50, 50,90,20);    
         f.add(cb);        
         f.setLayout(null);    
