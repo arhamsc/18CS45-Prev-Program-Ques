7696b40b86f61a1d1d2ef6adc14d3f16d6159c3d
SiddharthB70 <siddharth7002b@gmail.com>
2022-08-09 15:43:19 +0530

    Completed Swings Question 5

--- src/com/swingsQ5/StudentRecordTable.java
+++ src/com/swingsQ5/StudentRecordTable.java
@@ -1,5 +1,41 @@
+/*5. Write a swing program to create a table with column heading as StudentName, USN, Address
+and inset 5 Records into the table and display on screen. */
+
+
 package com.swingsQ5;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class StudentRecordTable {
+    JFrame f;
+    JTable j;
+
+    StudentRecordTable()
+    {
+        f = new JFrame();
+        f.setTitle("Student Records");
+
+        String[][] data = {
+            { "Motu", "1BH18CS001", "Yelahanka" },
+            { "Patlu", "1BH18CS023", "Vidyaranyapura"},
+            { "Raju", "1BH18CS069", "RT Nagar"},
+            { "Ravi", "1BH18CS420", "Whitefield"},
+            { "Krishna","1BH18CS004", "Koramangala"}
+
+        };
+
+        String[] columnNames = { "Student Name", "USN", "Address" };
+        j = new JTable(data, columnNames);
+        j.setBounds(30, 40, 200, 300);
+        JScrollPane sp = new JScrollPane(j);
+        f.add(sp);
+        f.setSize(500, 200);
+        f.setVisible(true);
+    }
+    public static void main(String[] args)
+    {
+        new StudentRecordTable();
+    }
     
 }
